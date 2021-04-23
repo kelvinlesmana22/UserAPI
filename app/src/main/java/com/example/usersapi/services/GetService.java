bcd4e232a725ed3dfa10f3de14cd5dd18b17fadf
kelvinlesmana22 <kelvinlesmana66@gmail.com>
2021-04-23 13:27:19 +0700

    UserAPI

---
+++ app/src/main/java/com/example/usersapi/services/GetService.java
@@ -0,0 +1,13 @@
+package com.example.usersapi.services;
+
+import com.example.usersapi.model.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetService {
+    @GET("/users")
+    Call<List<User>> getUsersList();
+}
